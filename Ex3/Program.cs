7f2c10d161d4fea06e88595bdb9f6365e5076df3
Kattebu <butskoev@tut.by>
2023-02-12 23:41:28 +0300

    Exercise3

---
+++ Ex3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Exercise3
+Console.Write("введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N % 2==0)
+{
+    Console.WriteLine("число: " + N + " является четным");
+}
+else
+{
+     Console.WriteLine("число: " + N + " не является четным");
+}
