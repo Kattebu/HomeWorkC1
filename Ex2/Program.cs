fd24b2d82af6b4ea4e69301a1ffd99577d68d16e
Kattebu <butskoev@tut.by>
2023-02-12 23:36:10 +0300

    Exercise2

---
+++ Ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Exercise2
+Console.Write("введите первое число: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите третье число: ");
+int thirdValue = Convert.ToInt32(Console.ReadLine());
+if ((firstValue > secondValue) && (firstValue > thirdValue))
+{
+    Console.WriteLine(" максимальное число: " + firstValue);
+}
+else
+{
+   if (secondValue > thirdValue)
+   {
+    Console.WriteLine(" максимальное число: " + secondValue);
+   }
+   else
+   {
+    Console.WriteLine(" максимальное число: " + thirdValue);
+   }
+}
