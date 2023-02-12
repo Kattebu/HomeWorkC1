177f6d485f487d0e2083f231ea2d78b2f1148db0
Kattebu <butskoev@tut.by>
2023-02-12 23:32:46 +0300

    Exercise1

---
+++ Ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Exersise1
+Console.Write("введите первое число: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+if (firstValue > secondValue)
+{
+    Console.WriteLine("максимальное число: " + firstValue + " минимальное число: " + secondValue);
+  
+}
+else
+{
+  Console.WriteLine(" минимальное число: " + firstValue + ", максимальное число: " + secondValue);   
+}
