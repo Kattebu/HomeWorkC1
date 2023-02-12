11cbae52f8f99ec9dee87a515835f8146d19906d
Kattebu <butskoev@tut.by>
2023-02-12 23:44:00 +0300

    Exercise4

---
+++ Ex4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Exercise4
+Console.Write("введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int A = 1; 
+while (A <= N)
+{
+   if (A%2==0)
+   {
+    Console.WriteLine(A);
+   }
+    A++; 
+}
